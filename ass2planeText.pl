2b09fbca6772397133fd8cd1f0c355ea1b4a4c6f
KentaGoto <gotokentag@gmail.com>
2021-07-07 13:31:30 +0900

    formatting

--- ass2planeText.pl
+++ ass2planeText.pl
@@ -3,15 +3,18 @@ use warnings;
 use utf8;
 use File::Find::Rule;
 
+
 print "Folder: ";
 chomp( my $dir = <STDIN> );
+
 my @ass = File::Find::Rule->file->name('*.ass')->in($dir);
 
 for my $ass (@ass){
 	( my $txt = $ass ) =~ s{\.ass$}{\.txt};
+
 	print $ass ."\n";
 
-	open my $in, '<', $ass or die $!;
+	open my $in,  '<', $ass or die $!;
 	open my $out, '>', $txt or die $!;
 
 	while ( my $l = <$in> ){
@@ -23,7 +26,6 @@ for my $ass (@ass){
 
 	close $in;
 	close $out;
-	
 }
 
 print "\nDone!\n";
